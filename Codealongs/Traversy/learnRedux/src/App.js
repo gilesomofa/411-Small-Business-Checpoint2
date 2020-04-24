0d7869e99eb342186f61c0c771b5c4210f2b1a21
Your Name <you@example.com>
2020-04-24 17:27:00 -0500

    project done

---
+++ Codealongs/Traversy/learnRedux/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Posts from './components/Posts';
+import Postform from './components/Postform';
+import  { Provider } from 'react-redux';
+import store from './store'
+
+function App() {
+  return (
+    <Provider store ={store}>
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <Postform />
+      <hr/>
+      <Posts />
+      
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
