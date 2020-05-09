29230ca8257edb751aa758ecf31df34fdc9e7682
Giles Smith <clintphillips@Giles-MacBook-Pro.local>
2020-05-08 19:05:44 -0500

    Removing node_modules from git history

---
+++ DailySped/dailysped-app/src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import './App.css';
+import { Provider } from 'react-redux';
+import Students from './components/Students';
+import StudentForm from './components/StudentForm';
+import store from './store';
+
+class App extends Component {
+render() {
+return (
+<Provider store={store}>
+<div className="App">
+<StudentForm />
+<hr />
+<Students />
+</div>
+</Provider>
+);
+}
+}
+export default App;
---
+++ DailySped/dailysped-app/src/actions.js/types.js
@@ -0,0 +1,3 @@
+
+export const FETCH_STUDENTS = 'FETCH_STUDENTS';
+export const NEW_STUDENT = 'NEW_STUDENT';
