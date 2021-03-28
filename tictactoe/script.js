fc336fe377ba6e2ba4dabdfdc9860daec859bbcf
Giles Smith <gilesomofa@yahoo.com>
2021-03-28 14:16:58 -0400

    first commit

---
+++ tictactoe/script.js
@@ -0,0 +1,37 @@
+//Tic Tac Toe
+// Elements of this game are
+//Game Board of 9 squares a grid of 3 x3
+//2 players x and o
+//a turn and change turn
+// a win state either player having 3 in a row vert. Hor. or diag.
+// pitfalls  player cannot move twice in a row, a square can only
+    //be occupied by one player(remove square from board)
+
+
+
+    function ticTacToe() { 
+        //
+        let rows = [1,2,3];
+        let columns = [1,2,3];
+        let startGame = func;
+        let input = '';
+        let player;
+        let playerX;
+        let playerO;
+        let win = func;
+
+        function board(){
+            for (let i = 1; i<4; i++){
+                for (letj =1; j<4; j++)
+                console.log(i,j)
+
+            }
+        }
+        // toggle player
+        // if(player === 'playerX'){
+        //     player = 'playerO'
+        // }else {
+        //     player = 'playerX'
+        // }
+}
+ 
